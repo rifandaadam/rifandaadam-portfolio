24634a8062fe315709d9a5d05a618721f4c39fc4
meokisama <hoangdinhsang123@gmail.com>
2020-12-26 11:42:00 +0700

    Update and change browser API

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+ **/
 
 export const onClientEntry = async () => {
   if (typeof IntersectionObserver === `undefined`) {
