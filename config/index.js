35de3c13e402c1084058222e905d1bc477b28bbe
meokisama <hoangdinhsang123@gmail.com>
2020-12-31 17:43:28 +0700

    Add more RSS

--- config/index.js
+++ config/index.js
@@ -52,7 +52,7 @@ module.exports = {
 
   // Similar to the Arrticle above
   nyaaTorrent:
-    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fotakukart.com%2Ffeed",
   shownTorrent: 3,
 
   //-- SOCIAL MEDIA SETTINGS -----
