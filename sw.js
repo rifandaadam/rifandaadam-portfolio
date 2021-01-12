972f24d9a95f3576c73634966fd88a6391d68b6e
meokisama <hoangdinhsang123@gmail.com>
2021-01-12 22:33:44 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-889cef628155cfbef689.js"
+    "url": "webpack-runtime-0e9ba22e7617dd1efa43.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5c936c1dd9d72bad2ac8898be28e0bbb"
+    "revision": "0f929deb6b6b87f6f477cc32574fe347"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-260e89c911c4cfae04b1.js"
