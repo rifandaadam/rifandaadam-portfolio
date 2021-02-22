9d424dd9edf270915ccd8d0e5c456a55f7426fc1
meokisama <hoangdinhsang123@gmail.com>
2021-02-22 14:27:22 +0700

    Update siteDescription

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   siteUrl: "https://meoki.net",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
-  seoTitleSuffix: "A devotee of Japanese culture", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  seoTitleSuffix: "Corner of a devotee of Japanese culture", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
   // -- THEME SETTINGS -----
   colors: {
