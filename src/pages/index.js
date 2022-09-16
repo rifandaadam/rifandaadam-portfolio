1f63c1b3311e90d2508e8467917be2b66e593d36
rifandaadam <47600046+rifandaadam@users.noreply.github.com>
2022-09-16 15:45:08 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ import GlobalStateProvider from "../context/provider"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/sections/hero"
-//import Articles from "../components/sections/articles"
-//import Techside from "../components/sections/techside"
+import Articles from "../components/sections/articles"
+import Techside from "../components/sections/techside"
 import About from "../components/sections/about"
 import Interests from "../components/sections/interests"
-//import Projects from "../components/sections/projects"
+import Projects from "../components/sections/projects"
 import Contact from "../components/sections/contact"
 import CreativeCoding from "../components/templates/creativecoding"
 import { seoTitleSuffix } from "../../config"
@@ -39,11 +39,11 @@ const IndexPage = ({ data }) => {
         />
         <Hero content={data.hero.edges} />
         {/* Articles is populated via Medium RSS Feed fetch */}
-        <!--<Articles />
-        <Techside />-->
+        <Articles />
+        <Techside />
         <About content={data.about.edges} />
         <Interests content={data.interests.edges} />
-        <!--<Projects content={data.projects.edges} />-->
+        <Projects content={data.projects.edges} />
         <CreativeCoding content={data.ccoding.edges} />
         <Contact content={data.contact.edges} />
       </Layout>
@@ -150,38 +150,38 @@ export const pageQuery = graphql`
         }
       }
     }
-    //projects: allMdx(
-      //filter: {
-       // fileAbsolutePath: { regex: "/index/projects/" }
-      //  frontmatter: { visible: { eq: true } }
-    //  }
-    //  sort: { fields: [frontmatter___position], order: ASC }
-   // ) {
-   //   edges {
-    //    node {
-     //     body
-       //   frontmatter {
-      //      title
-       //     category
-       //     emoji
-       //     external
-       //     github
-        //    screenshot {
-        //      childImageSharp {
-         //       fluid(maxWidth: 400, quality: 90) {
-        //          ...GatsbyImageSharpFluid
-        //        }
-        //      }
-       //     }
-      //      tags
-      //      position
-      //      buttonVisible
-       //     buttonUrl
-        //    buttonText
-      //    }
-     //   }
-   //   }
- //   }
+    projects: allMdx(
+      filter: {
+        fileAbsolutePath: { regex: "/index/projects/" }
+        frontmatter: { visible: { eq: true } }
+      }
+      sort: { fields: [frontmatter___position], order: ASC }
+    ) {
+      edges {
+        node {
+          body
+          frontmatter {
+            title
+            category
+            emoji
+            external
+            github
+            screenshot {
+              childImageSharp {
+                fluid(maxWidth: 400, quality: 90) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            tags
+            position
+            buttonVisible
+            buttonUrl
+            buttonText
+          }
+        }
+      }
+    }
     contact: allMdx(
       filter: { fileAbsolutePath: { regex: "/index/contact/" } }
     ) {
